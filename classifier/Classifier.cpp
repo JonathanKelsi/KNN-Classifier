4ff3c083e3c47da76a147e722e39e6f8e6090cc4
idokatzav <ido.k2005@gmail.com>
2022-07-31 14:15:49 +0300

    More updates in Classifier: parts classify method, choose the correct type

--- classifier/Classifier.cpp
+++ classifier/Classifier.cpp
@@ -1,16 +1,26 @@
 #include "Classifier.h"
 #include <memory>
+#include <map>
+#include <algorithm>
 
 void Classifier::classify(Classified& unclassified, Distance distance) {
     std::vector<double> distances;
     for (int i = 0; i < m_classifiedData.size(); i++) {
         distances.push_back(distance.distance(unclassified.data(), m_classifiedData[i]->data()));
     }
-    std::vector<Classified> selected;
+    std::map<std::string, int> m;
     for (int i = 1; i <= m_k; i++) {
-        selected.push_back(
+        //to do: get the number vector sorted.
+        std::string strType;
+        if (m.find(strType) == m.end()) {
+            m.insert(std::pair<std::string, int>(strType, 1));
+        } else {
+            m[strType] += 1;
+        }
     }
+    std::string st = std::max_element(m.begin(), m.end())->first;
     //continue next
+    unclassified.handle(st);
 }
 Classifier::Classifier(int k) {
     m_isInit = false;
